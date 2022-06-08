e1bb8667b2c8c6bedc0980e09ff5a6cb562c6652
Firmin Launay <hey@firminlaunay.me>
2022-06-08 08:58:33 +0200

    Try to implement monoalphabetic encryption

--- main.sh
+++ main.sh
@@ -4,6 +4,11 @@ rm -f ./fifo
 mkfifo ./fifo
 hname="$(hostname -s)"
 
+priv_key=$(cat "code.key")
+code_upper=$(echo "$priv_key" | tr "a-z" "A-Z")
+code_lower=$(echo "$priv_key" | tr "A-Z" "a-z")
+code="$code_upper$code_lower"
+
 function hash_password () {
     local pwd_hash_full=$(echo $1 | sha256sum)
     local pwd_hash=$(echo $pwd_hash_full | rev | cut -c3- | rev)
@@ -22,19 +27,27 @@ function test_password() {
     done
 }
 
+function decrypt() {
+  echo -n "$1" | tr "$code" "A-Za-z"
+}
+
+function crypt() {
+  echo -n "$1" | tr "A-Za-z" "$code"
+}
+
 function interpret () {
   current_date=$(LANG=en_us_88591; date)
 
-  echo $current_date
-  echo "Welcome to $hname!"
-  echo -n "Please enter your password: "
+  crypt "$current_date\n"
+  crypt "Welcome to $hname!\n"
+  crypt "Please enter your password: "
   pwd_file=$(cat password.conf)
 
 
 
-  while read line;
+  while read line_enc;
   do
-    # echo "Debug, received == $line ==" >&2
+    line=$(decrypt line_enc)
     for text_line in $pwd_file
     do
       pwd_hash=$(hash_password "$line")
@@ -48,23 +61,24 @@ function interpret () {
     done
     if [ $success == "1" ]
     then
-      echo "The password is correct. You are now logged in."
+      crypt "The password is correct. You are now logged in.\n"
       break
     else
-      echo -n "The password is incorrect. Please enter your password: "
+      crypt "The password is incorrect. Please enter your password: "
     fi
   done
   echo -n "$USER@$hname $PWD \$ "
-  while read line;
+  while read line_enc;
   do
-    $line
-    echo -n "$USER@$hname $PWD \$ "
+    line=$(decrypt line_enc)
+    $line | xargs -I{} crypt {}
+    crypt "$USER@$hname $PWD \$ "
   done
 }
 
 echo "[LOG] Server starting..."
 while true
 do
-  nc -l localhost 12345 < ./fifo | interpret &> ./fifo
+  nc -l 12345 < ./fifo | interpret &> ./fifo
 done
 echo "[LOG] Server ending..."
